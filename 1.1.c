4dd7400a913e14a2fd4db7a1d494348c9cb21e78
Alexis <ajligna@espe.edu.ec>
2026-04-10 10:03:42 -0500

    version1

---
+++ 1.1.c
@@ -0,0 +1,32 @@
+
+#include<stdio.h>
+int main()
+{
+    float a,b,c;
+    printf("bienvenido al programa de promedio");
+     do
+    {
+        printf("Ingrese el numero 1: ");
+        scanf("%f",&a);
+    }while(a<0 || a>20);
+    do
+    {
+        printf("Ingrese el numero 2: ");
+        scanf("%f",&b);
+    }while(b<0 || b>20);
+c=(a+b)/2;
+printf("el promedio es: %.2f\n",c);
+if (c >= 0 && c < 11)
+{
+    printf("perdido\n");
+}
+else if (c >= 11 && c < 14)
+{
+    printf("supletorio\n");
+}
+else
+{
+    printf("aprobado\n");
+}
+    return 0;
+}
